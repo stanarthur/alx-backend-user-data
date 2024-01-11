a90dccb47b39176d07e653227b56144bfa9a2cad
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2024-01-11 13:00:17 +0100

    5. Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A module for encrypting passwords"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hashes a password using a random salt"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
