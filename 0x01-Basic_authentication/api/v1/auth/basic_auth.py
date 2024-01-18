e59d7ea8c8e37f83b44957b3167d3749ec0425b3
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2024-01-18 22:52:16 +0100

    9. Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -48,7 +48,9 @@ class BasicAuth(Auth):
         """
         if base64_authorization_header is None or not isinstance(
                 base64_authorization_header, str):
-            """Return None if base64_authorization_header is None or not a str
+            """
+            Return None if base64_authorization_header is None
+            or not a str
             """
             return None
 
@@ -57,6 +59,34 @@ class BasicAuth(Auth):
             decoded_str = decoded_bytes.decode('utf-8')
             return decoded_str
         except Exception:
-            """Return None if base64_authorization_header is not a valid Base64
+            """
+            Return None if base64_authorization_header
+            is not a valid Base64
             """
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extracts the user email and password from the Base64 decoded value.
+
+        Args:
+            decoded_base64_authorization_header (str):
+            The decoded Base64 Authorization header.
+
+        Returns:
+            tuple: A tuple containing user email and password.
+        """
+        if (decoded_base64_authorization_header is None or
+                not isinstance(decoded_base64_authorization_header, str) or
+                ':' not in decoded_base64_authorization_header):
+            """
+            Return None, None if decoded_base64_authorization_header is None,
+            not a string, or doesn't contain : character or : is the first or
+            last character of decoded_base64_authorization_header
+            """
+            return None, None
+
+        user_email, user_password = decoded_base64_authorization_header.split(
+                                                                        ':', 1)
+        return user_email, user_password
