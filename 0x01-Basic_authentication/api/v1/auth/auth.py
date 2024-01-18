54750d308215bae0963cc3d82929a340499aa43b
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2024-01-18 13:13:32 +0100

    3. Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Checks if a path requires authentication.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """It returns False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """It returns None
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """It returns None
+        """
+        return None
