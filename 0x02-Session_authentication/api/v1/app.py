c5fd876c83f227bf14fe8ed1073b7b8a201e01f1
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2024-01-21 00:03:26 +0100

    5. update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,21 +36,17 @@ def authenticate_user():
         "/api/v1/status/",
         "/api/v1/unauthorized/",
         "/api/v1/forbidden/",
+        "/api/v1/auth_session/login/"
     ]
 
-    if auth.require_auth(request.path, excluded_paths):
-        user = auth.current_user(request)
-        if auth.authorization_header(request) is None and \
-                auth.session_cookie(request) is None:
-            abort(401)
-        if user is None:
-            abort(403)
-        request.current_user = user
-
-
-def require_auth():
-    excluded_paths = ['/api/v1/auth_session/login/']
-
+    # if auth.require_auth(request.path, excluded_paths):
+    #     user = auth.current_user(request)
+    #     if auth.authorization_header(request) is None and \
+    #             auth.session_cookie(request) is None:
+    #         abort(401)
+    #     if user is None:
+    #         abort(403)
+    #     request.current_user = user
     if request.path in excluded_paths:
         return
 
