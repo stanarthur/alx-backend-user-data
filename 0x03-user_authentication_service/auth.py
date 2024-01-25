ddd79ccd2234be7f8f0470fe15c00cf1a8782d00
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2024-01-25 21:39:24 +0100

    5. Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,10 @@
 """A module for authentication-related routines.
 """
 import bcrypt
+from user import User
+from bcrypt import checkpw
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -14,3 +18,20 @@ def _hash_password(password: str) -> bytes:
     """Hash the password with the salt
     """
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Adds a new user to the database.
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
