5a26ffb58f483a1899445d76ea001400d4c6f355
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2024-01-25 10:49:15 +0100

    2. fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a user instance to the session DB
         """
