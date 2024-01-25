ee9032d3b21dda4a15262dc7b44b303b6ae19780
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2024-01-25 10:15:16 +0100

    2. Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
 from user import Base, User
 
 
@@ -36,3 +39,16 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by the given query parameters
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+
+            if user is None:
+                raise NoResultFound("No user found with the given query parameters")
+
+            return user
+        except InvalidRequestError as e:
+            raise InvalidRequestError("Invalid query parameters") from e
