8160e622ac396f0a292b9ed683f84cdde6e456fc
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2024-01-26 01:05:39 +0100

    12. fix-2

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,7 +73,7 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
-    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+    def get_user_from_session_id(self, session_id: str) -> User:
         """Retrieves a user based on a given session ID.
         """
         user = None
